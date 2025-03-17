b2c5d40ffdaf96d2c542fb7d3dae1ca816be59f0
IcarusWings <icaromoura.programador@gmail.com>
2025-03-17 15:11:23 -0300

    feat: create header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,14 @@ import './style.css';
 
 export default function Header() {
   return (
-    <>
-      <h1>Header</h1>
-      <p>Onde sera colocado algumas informacoes</p>
-    </>
+    <div className="header">
+      <div className="title"><h1>Portfolio</h1></div>
+      <div className="links">
+      <div className="link"><a href="#about">About</a></div>
+      <div className="link"><a href="#projects">Projects</a></div>
+      <div className="link"><a href="#skills">Skills</a></div>
+      <div className="link"><a href="#contact">Contact</a></div>
+      </div>
+    </div>
   )
 }
